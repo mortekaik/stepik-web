275d7cba052cf32f8f24545fe408f331efefc029
mortekaik <astral.eskimo@gmail.com>
2019-05-13 00:27:40 +0300

    modified init and hello

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(env, start_response):
 	data = '\r\n'.join(env['QUERY_STRING'].split('&'))
 	start_response('200 OK', [('Content-Type', 'text/plain')])
-	return [data]
+	return data
--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 
-# sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-# sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 
 sudo /etc/init.d/gunicorn restart
