3a4b614aa517c75c8b669ca58d24db544835ae02
mortekaik <astral.eskimo@gmail.com>
2019-05-13 00:41:46 +0300

    modified conf files 2

--- init.sh
+++ init.sh
@@ -1,10 +1,21 @@
+#nginx conf
 sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
+#gunicorn conf
+sudo rm /etc/gunicorn.d/test
+# sudo rm /etc/gunicorn.d/ask
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-
-# sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-# sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
-
+# sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+#database conf
+#sudo /etc/init.d/mysql restart
+# mysql -uroot -e "DROP DATABASE ASK"
+# mysql -uroot -e "CREATE DATABASE ASK"
+# mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+# mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+# python /home/box/web/ask/manage.py syncdb
